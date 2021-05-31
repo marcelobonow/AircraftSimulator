0e16061ded6abde7c083c0206f77bfa40033765a
Marcelo Ott <marcelobonowott@hotmail.com>
2021-05-31 00:29:52 -0300

    Camera controls, more realistic mathematical model

--- Assets/Scripts/AircraftController.cs
+++ Assets/Scripts/AircraftController.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class AircraftController : MonoBehaviour
 {
     [SerializeField] private Transform runway;
+    [SerializeField] private TextMeshProUGUI horizontalSpeedText;
+    [SerializeField] private TextMeshProUGUI verticalSpeedText;
+    [SerializeField] private TextMeshProUGUI heightText;
+
     private Rigidbody rigidBody;
 
     private const float mass = 3000;
     private const float gravity = 9.8f;
     private const float dragCoeficient = 0.027f;
     private const float wingArea = 25.9f;
-    private const float liftCoeficient = 1.2f;
+    private const float liftCoeficient = 0.2f;
     private const float engineForce = mass * gravity * 0.5f;
-    private const float airDensity = 1.1455f;
+    private const float initialAirDensity = 1.1455f;
 
     private void Awake()
     {
@@ -22,6 +27,7 @@ public class AircraftController : MonoBehaviour
         Physics.gravity = new Vector3(0, -gravity, 0);
         rigidBody.mass = mass;
         rigidBody.drag = 0;
+        rigidBody.velocity = new Vector3(0, 0f, 100f);
         rigidBody.angularDrag = 0;
     }
 
@@ -29,7 +35,7 @@ public class AircraftController : MonoBehaviour
     {
         ///http://www.jaymaron.com/flight.html
         ///https://www.slideshare.net/sureshkrv/mel341-3
-        //At low altitudes above sea level, the pressure decreases by about 1.2 kPa (12 hPa) for every 100 metres
+        ///Colocando limite de 80km, e iniciando com densidade de 1.1455, temos que a densidade diminui 14,31875e-6 por metro
         ///Na equação:
         ///L = Lift
         ///CL = Angulo de ataque e formato da asa (constante no modelo)
@@ -46,7 +52,7 @@ public class AircraftController : MonoBehaviour
     private float GetDrag()
     {
         var velocity = rigidBody.velocity.z;
-        var drag = dragCoeficient * 0.5f * airDensity * Mathf.Pow(velocity, 2) * wingArea;
+        var drag = dragCoeficient * 0.5f * GetAirDensity() * Mathf.Pow(velocity, 2) * wingArea;
         Debug.Log("Velocity: " + velocity);
         Debug.Log("Drag: " + drag);
         return drag;
@@ -55,18 +61,29 @@ public class AircraftController : MonoBehaviour
     private float GetLift()
     {
         var velocity = rigidBody.velocity.z;
-        var lift = liftCoeficient * 0.5f * airDensity * Mathf.Pow(velocity, 2) * wingArea;
+        var lift = liftCoeficient * 0.5f * GetAirDensity() * Mathf.Pow(velocity, 2) * wingArea;
+        var verticalDrag = dragCoeficient * 0.5f * GetAirDensity() * Mathf.Pow(rigidBody.velocity.y, 2) * wingArea;
         Debug.Log("lift: " + lift);
-        return lift;
+        return lift - verticalDrag;
+    }
+
+    private float GetAirDensity()
+    {
+        var height = rigidBody.position.y;
+        var airDensityCalculated = initialAirDensity - (14.31875e-6 * height);
+        Debug.Log("Densidade do ar calculada: " + airDensityCalculated);
+        return Mathf.Clamp((float)airDensityCalculated, 0, initialAirDensity);
     }
 
     private void FixedUpdate()
     {
-        var force = engineForce - GetDrag();
+        var force = (engineForce * GetAirDensity()) - GetDrag();
         rigidBody.AddForce(new Vector3(0, GetLift(), force));
         if (runway.position.z <= transform.position.z - 4000f)
-        {
             runway.position = new Vector3(runway.position.x, runway.position.y, transform.position.z);
-        }
+
+        horizontalSpeedText.text = rigidBody.velocity.z + " m/s";
+        verticalSpeedText.text = rigidBody.velocity.y + " m/s";
+        heightText.text = rigidBody.position.y / 1000f + " km";
     }
 }
---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraRotation : MonoBehaviour
+{
+
+    [SerializeField] private float deadZone = 10f;
+    [SerializeField, Range(20f, 500f)] private float sensibility = 1;
+
+    private Vector2 initialPosition;
+    private bool swiping;
+    private bool clicked;
+
+    private void Update()
+    {
+        var overUI = EventSystem.current.IsPointerOverGameObject();
+        if (Input.GetMouseButtonDown(0) && !overUI)
+        {
+            initialPosition = Input.mousePosition;
+            clicked = true;
+            swiping = false;
+        }
+
+        if (Input.GetMouseButton(0) && clicked)
+        {
+            var swipeDistance = (Vector2)Input.mousePosition - initialPosition;
+            if (swipeDistance.magnitude > deadZone)
+            {
+                swiping = true;
+            }
+
+            if (swiping)
+            {
+                var mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * -1f);
+                RotateCamera(mouseMovement, sensibility);
+            }
+        }
+    }
+
+    public void RotateCamera(Vector3 mouseMovement, float sensibility)
+    {
+        var currentRotation = transform.rotation.eulerAngles;
+        currentRotation.y += mouseMovement.x * sensibility * Time.deltaTime;
+        currentRotation.x += mouseMovement.y * sensibility * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(currentRotation);
+    }
+}
